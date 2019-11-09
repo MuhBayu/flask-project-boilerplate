9e8061424a812a2d050d7290c1ff93d82608309d
unknown <bnugraha00@gmail.com>
2019-11-09 11:12:04 +0700

    update

--- controllers/indexController.py
+++ controllers/indexController.py
@@ -1,9 +1,6 @@
-import os
-from flask_restful import Resource, Api
+from flask_restful import Resource
 from flask import request
 
 class HelloWorld(Resource):
     def get(self):
-        id = os.getenv('APP_ID')
-        print(id)
-        return {'hello': 'world'}
+        return {'status': 200}
